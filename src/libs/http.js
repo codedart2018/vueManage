48baab110b42b8ea0b7f2cbae984b0c88d812013
myxingke <myxingke@126.com>
2017-04-19 21:37:35 +0800

    Http 请求改写成插件方式

--- src/libs/http.js
+++ src/libs/http.js
@@ -1,68 +1,71 @@
 /**
- * Created by truncate on 2017/3/28.
+ * Created by truncate on 2017/4/19.
+ * 网络请求插件
  */
 
-import AxiosInst from './axios'
 import methodMap from './methodMap'
+import AxiosInst from './axios'
 
-const apiHttp = {
-    methods: {
-        request(method, opts) {
-            let m = methodMap[method]
-            if (m) {
-                var opts_type = typeof(opts);
-                if (opts_type == null || opts_type != 'object') {
-                    opts = {}
-                }
-                if(m.method == 'get') {
-                    return this.apiGet(m.url, opts)
-                } else if(m.method == 'post') {
-                    return this.apiPost(m.url, opts)
-                } else {
-                    return "非法请求";
-                }
+class Http {
 
-            } else {
-                console.log("url 错误", "返回结果：err = ", "无法请求，无效的请求！", "\n");
+}
+
+Http.install = function (Vue) {
+    Vue.prototype.$msg = 'Hello World';
+
+    Vue.prototype.request = function (method, opts) {
+        let m = methodMap[method]
+        if (m) {
+            var opts_type = typeof(opts);
+            if (opts_type == null || opts_type != 'object') {
+                opts = {}
             }
-        },
-        apiPost(url, data) {
-            return new Promise((resolve, reject) => {
-                AxiosInst.post(url, data).then((response) => {
-                    if(response.data == null) {
-                        console.log("接口输出异常...")
-                        this.$Message.error("接口输出异常...")
-                        return
-                    }
-                    resolve(response.data)
-                }).catch((response) => {
-                    console.log('Customize Notice', response)
-                    reject(response)
-                })
-            })
-        },
-        apiGet(url, data) {
-            return new Promise((resolve, reject) => {
-                AxiosInst.get(url, {
-                    params: data
-                }).then((response) => {
-                    if(response.data == null) {
-                        console.log("接口输出异常...")
-                        this.$Message.error("接口输出异常...")
-                        return
-                    }
-                    resolve(response.data)
-                }).catch((response) => {
-                    console.log('Customize Notice', response)
-                    reject(response)
-                })
-            })
-        },
-    },
-    computed: {
+            if(m.method == 'get') {
+                return Vue.prototype.apiGet(m.url, opts)
+            } else if(m.method == 'post') {
+                return Vue.prototype.apiPost(m.url, opts)
+            } else {
+                return "非法请求";
+            }
 
+        } else {
+            console.log("url 错误", "返回结果：err = ", "无法请求，无效的请求！", "\n");
+        }
+    }
+
+    Vue.prototype.apiPost = function(url, data) {
+        return new Promise((resolve, reject) => {
+            AxiosInst.post(url, data).then((response) => {
+                if(response.data == null) {
+                    console.log("接口输出异常...")
+                    this.$Message.error("接口输出异常...")
+                    return
+                }
+                resolve(response.data)
+            }).catch((response) => {
+                console.log('Customize Notice', response)
+                reject(response)
+            })
+        })
+    }
+
+    Vue.prototype.apiGet = function(url, data) {
+        return new Promise((resolve, reject) => {
+            AxiosInst.get(url, {
+                params: data
+            }).then((response) => {
+                if(response.data == null) {
+                    console.log("接口输出异常...")
+                    this.$Message.error("接口输出异常...")
+                    return
+                }
+                resolve(response.data)
+            }).catch((response) => {
+                console.log('Customize Notice', response)
+                reject(response)
+            })
+        })
     }
 }
 
-export default apiHttp
-
+export default Http
---
+++ src/libs/http_scrap.js
@@ -0,0 +1,69 @@
+/**
+ * Created by truncate on 2017/3/28.
+ * 此文件已废弃
+ */
+
+import AxiosInst from './axios'
+import methodMap from './methodMap'
+
+const apiHttp = {
+    methods: {
+        request(method, opts) {
+            let m = methodMap[method]
+            if (m) {
+                var opts_type = typeof(opts);
+                if (opts_type == null || opts_type != 'object') {
+                    opts = {}
+                }
+                if(m.method == 'get') {
+                    return this.apiGet(m.url, opts)
+                } else if(m.method == 'post') {
+                    return this.apiPost(m.url, opts)
+                } else {
+                    return "非法请求";
+                }
+
+            } else {
+                console.log("url 错误", "返回结果：err = ", "无法请求，无效的请求！", "\n");
+            }
+        },
+        apiPost(url, data) {
+            return new Promise((resolve, reject) => {
+                AxiosInst.post(url, data).then((response) => {
+                    if(response.data == null) {
+                        console.log("接口输出异常...")
+                        this.$Message.error("接口输出异常...")
+                        return
+                    }
+                    resolve(response.data)
+                }).catch((response) => {
+                    console.log('Customize Notice', response)
+                    reject(response)
+                })
+            })
+        },
+        apiGet(url, data) {
+            return new Promise((resolve, reject) => {
+                AxiosInst.get(url, {
+                    params: data
+                }).then((response) => {
+                    if(response.data == null) {
+                        console.log("接口输出异常...")
+                        this.$Message.error("接口输出异常...")
+                        return
+                    }
+                    resolve(response.data)
+                }).catch((response) => {
+                    console.log('Customize Notice', response)
+                    reject(response)
+                })
+            })
+        }
+    },
+    computed: {
+
+    }
+}
+
+export default apiHttp
+
