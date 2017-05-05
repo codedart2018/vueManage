352842fbcc5b0a31336b5d794ae03d0a394ccbe2
myxingke <myxingke@126.com>
2017-05-05 16:49:32 +0800

    百度编辑器组件 测试成功

---
+++ src/pages/manage/merchant/index.vue
@@ -0,0 +1,24 @@
+<!--商户列表组件-->
+<template>
+    <div>
+        <div>商户列表组件</div>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
--- src/test/Store.vue
+++ src/test/Store.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <div>this is template body</div>
+        <div>this is state template body</div>
+        <UEditor ref="editor" @ready="editorReady" style="width: 500px;height: 440px;" :config="{hello: 1}"></UEditor>
+        <button @click="setContent">追加内容</button>
     </div>
 </template>
 
 <script>
     import Vue from 'vue'
     import Vuex from 'vuex'
+    import UEditor from '../components/editor'
     Vue.use(Vuex)
+
     const store = new Vuex.Store({
         state: {
             count: 0
@@ -19,16 +23,31 @@
             }
         }
     })
+
     export default{
         data(){
-            return{
-                msg:'hello vue'
+            return {
+                msg: 'hello vue'
             }
         },
         store,
         mounted() {
             store.commit('increment', 5)
             console.log(store.state.count)
+        },
+        methods: {
+            editorReady(instance) {
+                instance.setContent('hello world');
+                instance.addListener('contentChange', () => {
+                    this.content = instance.getContent();
+                });
+            },
+            setContent() {
+                this.$refs.editor.setContent('这是重新设置的内容')
+            }
+        },
+        components: {
+            UEditor
         }
     }
 </script>
